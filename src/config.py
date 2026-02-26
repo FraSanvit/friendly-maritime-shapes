2727c6b5f9fc2526ce5e53b84e2ca81df88a3aa6
Francesco Sanvito <68587472+FraSanvit@users.noreply.github.com>
2026-02-26 15:57:39 +0100

    Add output CRS to the config

--- src/config.py
+++ src/config.py
@@ -2,11 +2,11 @@
 
 from pathlib import Path
 
+CRS = "EPSG:4326"
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 
 scenario_name = "ehighways"
-
-ref_shape_path = PROJECT_ROOT / f"resource/{scenario_name}/ref_shape.parquet"
+ref_shape_path = PROJECT_ROOT / f"resource/{scenario_name}/ref_shapes.parquet"
 custom_shape_path = PROJECT_ROOT / f"resource/{scenario_name}/shapes.parquet"
 result_shape_path = PROJECT_ROOT / f"results/{scenario_name}/{scenario_name}.parquet"
 
